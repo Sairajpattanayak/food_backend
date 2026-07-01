4f67abc33b9d04d54824d015db00f7485a7c4f27
sairaj pattanayak <sairajpattanayak38@gmail.com>
2026-07-01 13:45:15 +0530

    hii

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ import TableRoutes from "./routes/table.routes.js";
 const app = express();
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://food-frontend-95cw.vercel.app/",
         credentials: true,
     }),
 );
